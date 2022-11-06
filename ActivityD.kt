23548d9f89ef664a4fe93c23a41a40a86c31ff33
Дмитрий Моисеенко <113856132+Cnstrctr1@users.noreply.github.com>
2022-11-06 21:34:08 +0300

    Add files via upload
    Classes A, B, C, D

---
+++ ActivityD.kt
@@ -0,0 +1,15 @@
+package Activity_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.MainActivity
+import com.example.myapplication.R
+
+class ActivityD: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d)
+    }
+}
